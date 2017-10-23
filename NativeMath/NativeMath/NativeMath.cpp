908b957b95904405792ea5c37337fc0ab728cff6
James Swift <james@pspdfkit.com>
2017-10-23 15:32:04 +0200

    Walk-through solution

---
+++ NativeMath/NativeMath/NativeMath.cpp
@@ -0,0 +1,20 @@
+﻿// NativeMath.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "pch.h"
+#include "NativeMath.h"
+
+
+BasicMath::BasicMath()
+{
+}
+
+double BasicMath::add(double firstNumber, double secondNumber)
+{
+    return firstNumber + secondNumber;
+}
+
+double BasicMath::subtract(double firstNumber, double secondNumber)
+{
+    return firstNumber - secondNumber;
+}
